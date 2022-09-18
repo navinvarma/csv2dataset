c9f1a693c073af7ae7cabb444ab678fed35d5188
Navin Varma <navinvarma@gmail.com>
2022-09-17 23:01:20 -0700

    Initial commit of project

---
+++ src/test/java/com/opendata/csvdb/csv2table/Csv2tableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opendata.csvdb.csv2table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csv2tableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
